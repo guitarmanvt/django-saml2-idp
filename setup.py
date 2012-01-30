f244d74510bb000e9f3f6b45563d15d5e34cdf68
john@andersoninnovative.com <john@andersoninnovative.com@41e50629-eb6e-b841-421f-ad03d4eac035>
2012-01-30 16:01:34 +0000

    Make requirement M2Crypto>=0.20.1. Issue #8

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name = 'saml2idp',
-    version = '0.14',
+    version = '0.15',
     author = 'John Samuel Anderson',
     author_email = 'john@andersoninnovative.com',
     description = 'SAML 2.0 IdP for Django',
     long_description = 'SAML 2.0 Identity Provider app for Django projects.',
-    install_requires = ['M2Crypto==0.20.1'],
+    install_requires = ['M2Crypto>=0.20.1'],
     license = 'MIT',
     packages = ['saml2idp', 'saml2idp.tests'],
     package_dir = {'saml2idp': 'idptest/saml2idp'},
